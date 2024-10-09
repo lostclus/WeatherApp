81acedcc860ae20697b4294a4ed4e91b757beefb
Konstantin Korikov <lostclus@gmail.com>
2024-10-09 11:54:24 +0300

    Fix admin

--- core/weatherapp_core/users/admin.py
+++ core/weatherapp_core/users/admin.py
@@ -1,9 +1,50 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as OrigUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from .models import User
 
 
 @admin.register(User)
 class UserAdmin(OrigUserAdmin):
+    list_display = ("email", "is_staff", "is_superuser", "is_active")
     ordering = ("email",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+        (
+            _("Settings"),
+            {
+                "fields": (
+                    "timezone",
+                    "temperature_unit",
+                    "wind_speed_unit",
+                    "precipitation_unit",
+                    "date_format",
+                    "time_format",
+                ),
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "usable_password", "password1", "password2"),
+            },
+        ),
+    )
